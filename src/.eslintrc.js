4acdc816fb1a6bd373d7020af9397d9c3a7b51a2
Renaud Tertrais <renaud.tertrais@gmail.com>
2017-01-29 20:57:27 +0100

    update src/eslint rules

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -2,6 +2,10 @@ module.exports = {
   plugins: ["react"],
   rules: {
     "react/prefer-es6-class": 0,
+    "react/forbid-prop-types": 0,
+    "react/no-danger": 0,
+    "react/jsx-no-target-blank": 0,
+    "react/require-default-props": 0,
     "import/no-extraneous-dependencies": 2,
   },
 };
